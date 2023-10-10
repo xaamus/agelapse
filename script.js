adaf08a14d8c785a6acc7bcbe754148342b0ab02
xaamus <kacper.samus@gmail.com>
2023-10-10 22:54:39 +0200

    added cookies func

--- script.js
+++ script.js
@@ -74,10 +74,25 @@ function timeLapsing(){
         }
     },1)
 }
-//timeLapsing();
+timeLapsing();
 
-let o = prompt()
-if (o == '2'){
-    document.cookie = "data=kupa;expires=Tue, 10 Oct 2023 19:252:00 UTC;path=/";
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-alert(document.cookie)
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
